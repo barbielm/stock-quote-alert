0692eceb9eb93a0e0237237ed43301bb49b16b3b
Gabriel Barbosa <barbosagabriel0508@gmail.com>
2022-09-03 08:02:48 -0300

    retrieving data from API

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string API_KEY = "fe9b819b";
+string informations = await Functions.GetStockPrice(API_KEY);
+Console.WriteLine(informations);
+Console.ReadLine();
+
+public class Functions
+{
+    public static async Task<string> GetStockPrice(string apiKey)
+    {
+        string uri = $"https://api.hgbrasil.com/finance/stock_price?key={apiKey}&symbol=PETR4";
+        var httpClient = new HttpClient();
+        var request = new HttpRequestMessage();
+        var response = await httpClient.GetAsync(uri);
+        var data = await response.Content.ReadAsStringAsync();
+        return data;
+    }
+    public static void checkBoundaries()
+    {
+
+    }
+}
