08a0ab78241186707a2bb94f2695d99ea7410f47
Gabriel Barbosa <barbosagabriel0508@gmail.com>
2022-09-03 14:50:02 -0300

    environment variables

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
 ﻿using System.Globalization;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+DotNetEnv.Env.Load();
+string? USER_EMAIL = Environment.GetEnvironmentVariable("USER");
+string? USER_PASSWORD = Environment.GetEnvironmentVariable("PASSWORD");
+string? API_KEY = Environment.GetEnvironmentVariable("API_KEY");
+
+if (API_KEY is null || USER_EMAIL is null || USER_PASSWORD is null)
+{
+    throw new ArgumentException("fill in your .env file accordingly", nameof(API_KEY));
+}
 
 string ASSET = args[0];
 float SELLING_PRICE = float.Parse(args[1], CultureInfo.InvariantCulture.NumberFormat);
 float BUYING_PRICE = float.Parse(args[2], CultureInfo.InvariantCulture.NumberFormat);
-string API_KEY = "fe9b819b";
-Functions.checkIfIntervalIsValid(BUYING_PRICE, SELLING_PRICE);
+
+
+Functions.CheckIfIntervalIsValid(BUYING_PRICE, SELLING_PRICE);
 string informations = await Functions.GetStockPrice(API_KEY);
 Console.WriteLine(informations);
 Console.ReadLine();
@@ -21,14 +36,14 @@ public class Functions
         return data;
     }
 
-    public static void checkIfIntervalIsValid(float buyingPrice, float sellingPrice)
+    public static void CheckIfIntervalIsValid(float buyingPrice, float sellingPrice)
     {
         if (sellingPrice <= buyingPrice)
         {
             throw new ArgumentException("selling price must always be higher than the buying price", nameof(sellingPrice));
         }
     }
-    public static void checkBoundaries(int buyingPrice, int sellingPrice, int currentPrice)
+    public static void CheckBoundaries(int buyingPrice, int sellingPrice, int currentPrice)
     {
         if (currentPrice > sellingPrice)
         {
@@ -41,7 +56,7 @@ public class Functions
         }
     }
 
-    public static void sendEmail()
+    public static void SendEmail(string userEmail, string password, string recipientEmail, string message, string subject)
     {
 
     }
